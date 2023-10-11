85a4c3ede74fceb9f18332c7d0e835a57bb7f889
CagiestSky99830 <120144559+CagiestSky99830@users.noreply.github.com>
2023-10-10 20:08:30 -0500

    Add files via upload

---
+++ c101-project/roll-a-dice.py
@@ -0,0 +1,44 @@
+import random
+
+answer = input("do you want to roll a dice? ")
+if answer == "yes" or "y":
+    number = random.randint(1,6)
+    if number == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if number == 2:
+        print("[-----]")
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("[-----]")
+    if number == 3:
+        print("[-----]")
+        print("[    0]")
+        print("[  0  ]")
+        print("[0    ]")
+        print("[-----]")
+
+    if number == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if number == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if number == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+if answer == "no" or "n":
+    print("stopped")
